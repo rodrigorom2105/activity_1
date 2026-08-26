001137c80353f0d45cfa8e546ca7bb804aecdb33
Rodrigo Romero <rocobros21@gmail.com>
2026-08-26 08:06:17 -0600

    Finished

--- components/RequestComponent.tsx
+++ components/RequestComponent.tsx
@@ -1,10 +1,44 @@
-import { useState, useEffect } from 'react'
-import { FlatList, View } from 'react-native'
+import { useState, useEffect } from "react";
+import { FlatList, View, Text, Pressable } from "react-native";
 
 export default function RequestComponent(props: any) {
-  const [data, setData] = useState({})
+  const [data, setData] = useState([]);
 
   async function request() {
-    var response = await 
+    var response = await fetch(props.url);
+    var json = await response.json();
+    setData(json.planetas);
   }
+
+  function showInfo(item: any) {
+    alert(
+      `Nombre: ${item.nombre}\nDiametro: ${item.diametro_km}\nDistancia al sol: ${item.distancia_al_sol_km}\nAnillos: ${item.tiene_anillos}\nLunas: ${item.lunas}`,
+    );
+  }
+
+  useEffect(() => {
+    request();
+  }, []);
+
+  return (
+    <View>
+      {data.length > 0 ? (
+        <FlatList
+          data={data}
+          keyExtractor={(item) => item["nombre"]}
+          renderItem={({ item }) => {
+            return (
+              <View>
+                <Pressable onPress={() => showInfo(item)}>
+                  <Text>{item["nombre"]}</Text>
+                </Pressable>
+              </View>
+            );
+          }}
+        />
+      ) : (
+        <Text>Cargando...</Text>
+      )}
+    </View>
+  );
 }
