b3c20738358f29c7aaa01d519ada876ff8ff0a5f
Rodrigo Romero <rocobros21@gmail.com>
2026-08-26 07:40:11 -0600

    Initial commit

---
+++ components/RequestComponent.tsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from 'react'
+import { FlatList, View } from 'react-native'
+
+export default function RequestComponent(props: any) {
+  const [data, setData] = useState({})
+
+  async function request() {
+    var response = await 
+  }
+}
